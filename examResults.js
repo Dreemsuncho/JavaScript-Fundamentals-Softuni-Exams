d76cdb420b8a3aa855ef85196a500f2df0e6fe04
dreemsuncho <dreemsuncho@gmail.com>
2017-02-08 20:04:34 +0200

    javascript update exams

--- examResults.js
+++ examResults.js
@@ -1,5 +1,43 @@
-function exam(arr) {
-    let res = arr.join(' ').split(' ');
+function exams(arr) {
+    let courseName = arr.pop().trim();
+    let students = new Map();
 
-    console.log(res)
+    while (arr.length > 0) {
+        let row = arr.shift().split(/\s+/).filter(x => x != '');
+
+        let student = row[0];
+        let course = row[1];
+        let examPoints = Number(row[2]);
+        let bonus = Number(row[3]);
+
+
+        let coursePoints = ((examPoints / 5) + bonus)
+            .toFixed(2).replace(/(\.0+$)|(0+$)/, '');
+
+        let grade = (((((examPoints / 5) + bonus) / 80) * 4) + 2).toFixed(2);
+
+        students.set([student], {
+            'courseName': course,
+            'examPoints': examPoints,
+            'coursePoints': coursePoints,
+            'grade': grade > 6 ? '6.00' : grade
+        });
+    }
+
+
+    let average = 0;
+    let count = 0;
+    students.forEach((st, [name]) => {
+        st.examPoints < 100 ?
+            console.log(`${name} failed at "${st.courseName}"`)
+            : console.log(`${name}: Exam - "${st.courseName}"; Points - ${st.coursePoints}; Grade - ${st.grade}`);
+
+        if (st.courseName === courseName) {
+            average += st.examPoints;
+            count++;
+        }
+    });
+
+    average = (average / count).toFixed(2).replace(/(\.0+$)|(0+$)/, '');
+    console.log(`"${courseName}" average points -> ${average}`);
 }
